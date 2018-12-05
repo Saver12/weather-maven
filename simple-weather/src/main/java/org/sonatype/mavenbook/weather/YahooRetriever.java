520fbd9ae7356a7249b149b44e3ae5e4cead0773
Sergei Ermushin <Sergei_Ermushin@epam.com>
2018-12-05 14:00:10 +0300

    refactor

--- simple-weather/src/main/java/org/sonatype/mavenbook/weather/YahooRetriever.java
+++ simple-weather/src/main/java/org/sonatype/mavenbook/weather/YahooRetriever.java
@@ -14,7 +14,7 @@ class YahooRetriever {
         log.info("Retrieving Weather Data");
         String url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20" +
                 "where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20" +
-                "where%20text%3D%22" + city.replaceAll("\\s+", "%20") + "%22)&format=xml&env=" +
+                "where%20text%3D%22" + city.trim().replaceAll("\\s+", "%20") + "%22)&format=xml&env=" +
                 "store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
         URLConnection conn = new URL(url).openConnection();
         return conn.getInputStream();
